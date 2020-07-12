cf7660d9d7b1e2a973e512b39e477577aa196b9e
Bekzhan Bershimbekov <Bekzhan99@gmail.com>
2020-07-13 01:18:37 +0600

    Extend the question form to be able to update question

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -44,6 +44,7 @@ class QuestionsController extends Controller
     }
 
     /**
+     *
      * Display the specified resource.
      *
      * @param  \App\Question  $question
@@ -62,7 +63,7 @@ class QuestionsController extends Controller
      */
     public function edit(Question $question)
     {
-        //
+        return view('questions.edit', compact('question'));
     }
 
     /**
@@ -72,9 +73,11 @@ class QuestionsController extends Controller
      * @param  \App\Question  $question
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Question $question)
+    public function update(AskQuestionRequest $request, Question $question)
     {
-        //
+        $question->update($request->only('title','body'));
+
+        return redirect()->route('questions.index')->with('success','Your question has been updated');
     }
 
     /**
