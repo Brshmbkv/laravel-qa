d588043c0d856e7989d23a62262c4241ab4b84c0
Bekzhan Bershimbekov <Bekzhan99@gmail.com>
2020-07-13 19:36:44 +0600

    Generate fake answers to questions\

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,7 +14,11 @@ class DatabaseSeeder extends Seeder
         factory('App\User', 3)->create()->each(function ($u){
             $u->questions()->saveMany(
                 factory('App\Question',rand(1,4))->make()
-            );
+            )->each(function($q){
+                $q->answers()->saveMany(
+                    factory('App\Answer', rand(1,5))->make()
+                );
+            });
         });
 
     }
