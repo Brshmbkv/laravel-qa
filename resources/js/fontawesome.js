f5d93f85cb4cea31c4fb5fdaf95a5a6b8fe67b72
Bekzhan Bershimbekov <Bekzhan99@gmail.com>
2020-07-14 18:34:06 +0600

    Add vote controls to question and and answer item

---
+++ resources/js/fontawesome.js
@@ -0,0 +1,6 @@
+import {library, dom} from "@fortawesome/fontawesome-svg-core";
+import {faCaretUp, faCaretDown, faCheck, faStar} from "@fortawesome/free-solid-svg-icons";
+
+library.add([faCaretUp,faCaretDown,faCheck,faStar]);
+
+dom.watch();
